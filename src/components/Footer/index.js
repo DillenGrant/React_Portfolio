11865a09da90483381cc1d4e79e85b3b6fdd3978
DillenGrant <dillen.grant72@gmail.com>
2024-01-24 23:03:02 -0700

    maybe dis work?

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGithub, FaLinkedin, FaBehanceSquare, FaInstagram } from "react-icons/fa";
+import { FaGithub, FaLinkedin} from "react-icons/fa";
 
 function Footer() {
   return (
